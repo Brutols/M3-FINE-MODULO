7b2d146caeced459089a5273f22aea0f61549f2b
Brutols <dtwo97@gmail.com>
2023-12-05 16:07:22 +0100

    netflix clone - navbar + card

---
+++ script.js
@@ -0,0 +1,110 @@
+const films = [
+    {
+      title: "Inception",
+      genre: "Science Fiction",
+      length: 148,
+      image: "./movies/1.png"
+    },
+    {
+      title: "The Shawshank Redemption",
+      genre: "Drama",
+      length: 142,
+      image: "./movies/2.png"
+    },
+    {
+      title: "The Dark Knight",
+      genre: "Action",
+      length: 152,
+      image: "./movies/3.png"
+    },
+    {
+      title: "Pulp Fiction",
+      genre: "Crime",
+      length: 154,
+      image: "./movies/4.png"
+    },
+    {
+      title: "Forrest Gump",
+      genre: "Drama",
+      length: 142,
+      image: "./movies/5.png"
+    },
+    {
+      title: "The Matrix",
+      genre: "Science Fiction",
+      length: 136,
+      image: "./movies/6.png"
+    },
+    {
+      title: "Titanic",
+      genre: "Romance",
+      length: 195,
+      image: "./movies/7.png"
+    },
+    {
+      title: "Jurassic Park",
+      genre: "Adventure",
+      length: 127,
+      image: "./movies/8.png"
+    },
+    {
+      title: "The Godfather",
+      genre: "Crime",
+      length: 175,
+      image: "./movies/9.png"
+    },
+    {
+      title: "Avatar",
+      genre: "Science Fiction",
+      length: 162,
+      image: "./movies/10.png"
+    },
+    {
+      title: "The Lord of the Rings: The Fellowship of the Ring",
+      genre: "Fantasy",
+      length: 178,
+      image: "./movies/11.png"
+    },
+    {
+      title: "Eternal Sunshine of the Spotless Mind",
+      genre: "Romance",
+      length: 108,
+      image: "./movies/12.png"
+    },
+    {
+      title: "The Silence of the Lambs",
+      genre: "Thriller",
+      length: 118,
+      image: "./movies/13.png"
+    },
+    {
+      title: "The Social Network",
+      genre: "Drama",
+      length: 120,
+      image: "./movies/14.png"
+    },
+    {
+      title: "Inglourious Basterds",
+      genre: "War",
+      length: 153,
+      image: "./movies/15.png"
+    },
+    {
+      title: "The Shawshank Redemption",
+      genre: "Drama",
+      length: 142,
+      image: "./movies/16.png"
+    },
+    {
+      title: "Interstellar",
+      genre: "Science Fiction",
+      length: 169,
+      image: "./movies/17.png"
+    },
+    {
+      title: "The Grand Budapest Hotel",
+      genre: "Comedy",
+      length: 100,
+      image: "./movies/18.png"
+    }
+  ];
